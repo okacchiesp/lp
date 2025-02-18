4ab78f476836b94717009403ca1b80a4a5e6d29b
Yuya Okano <Okacchi@Yuya-no-MacBook-Pro.local>
2025-02-19 00:20:29 +0900

    0219

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,21 @@
+jQuery(function ($) {});
+document.addEventListener("DOMContentLoaded", () => {
+    const fadeElements = document.querySelectorAll(".fade-in");
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("active");
+                }
+            });
+        },
+        {
+            threshold: 0.2, // 要素が20%表示されたら発火
+        }
+    );
 
-
+    fadeElements.forEach((element) => {
+        observer.observe(element);
+    });
 });
